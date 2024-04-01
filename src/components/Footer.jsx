27ddfbeb76201065d9c0e4086ed1f8e5cd2eed0a
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-03-31 22:54:36 -0300

    feat: create component Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,12 @@
 import styles from "./Footer.module.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <h3>Escreva sobre o que você tem interesse!</h3>
+      <p>Mini Blog &copy; 2024</p>
+    </footer>
+  );
 };
 
 export default Footer;
