8e966ed355aa168d4b146f8162b6519e93bb10f6
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-04-03 18:55:34 -0300

    feat: add function to create new User

--- src/hooks/useAuthentication.jsx
+++ src/hooks/useAuthentication.jsx
@@ -7,6 +7,8 @@ import {
 } from "firebase/auth";
 import { useEffect, useState } from "react";
 
+import { db } from "../firebase/config";
+
 export const useAuthentication = () => {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(null);
@@ -19,4 +21,48 @@ export const useAuthentication = () => {
       return;
     }
   }
+
+  const createUser = async (data) => {
+    checkIfIsCancelled();
+    setLoading(true);
+    setError(null);
+
+    try {
+      const { user } = await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password,
+      );
+
+      await updateProfile(user, { displayName: data.displayName });
+
+      setLoading(false);
+
+      return user;
+    } catch (error) {
+      let systemErrorMessage;
+
+      if (error.message.includes("Password")) {
+        systemErrorMessage = "A senha precisa ter pelo menos 6 caracteres.";
+      } else if (error.message.includes("email-already")) {
+        systemErrorMessage = "E-mail já cadastrado.";
+      } else {
+        systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde.";
+      }
+
+      setLoading(false);
+      setError(systemErrorMessage);
+    }
+  };
+
+  useEffect(() => {
+    return () => setCancelled(true);
+  }, []);
+
+  return {
+    auth,
+    createUser,
+    error,
+    loading,
+  };
 };
