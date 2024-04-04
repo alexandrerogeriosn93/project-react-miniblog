65279a4ad288dd74b6694dac8fe5e04464c4b59f
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-04-03 21:51:23 -0300

    feat: create routes to authenticated user

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import Navbar from "./components/Navbar";
 import { AuthProvider } from "./context/AuthContext";
 import { useAuthentication } from "./hooks/useAuthentication";
 import About from "./pages/About/About";
+import CreatePost from "./pages/CreatePost/CreatePost";
+import Dashboard from "./pages/Dashboard/Dashboard";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
@@ -38,6 +40,8 @@ function App() {
               <Route path="/about" element={<About />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
+              <Route path="/posts/create" element={<CreatePost />} />
+              <Route path="/dashboard" element={<Dashboard />} />
             </Routes>
           </div>
           <Footer />
