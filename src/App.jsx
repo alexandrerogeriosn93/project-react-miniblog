3522db74fb09dead89534a9ca3af7ed9aaec1e70
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-04-07 04:16:15 -0300

    feat: create function to search posts

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import Dashboard from "./pages/Dashboard/Dashboard";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
+import Search from "./pages/Search/Search";
 
 function App() {
   const [user, setUser] = useState(undefined);
@@ -38,6 +39,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
+              <Route path="/search" element={<Search />} />
               <Route
                 path="/login"
                 element={!user ? <Login /> : <Navigate to="/" />}
